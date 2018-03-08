e7da586f54c461bab02d602d4c5ee93b46986c5c
Azhypa <36773681+Azhypa@users.noreply.github.com>
2018-03-08 21:52:20 +0300

    Add files via upload

--- index.ts
+++ index.ts
@@ -3,50 +3,49 @@ import prompt from "@jace1995/readline";
 let score: number = 0;
 
 function generateKey(): string {
-    const char = ['+', '-', '*', '/'];
-    const key = Math.floor( Math.random() * 4 );
-   return char[key];
+  const char = ['+', '-', '*', '/'];
+  const key = Math.floor( Math.random() * 4 );
+  return char[key];
 }
 
 function operation(num1: number, num2: number, key: string): number {
-        switch (key) {
-            case '+':
-                return num1 + num2; 
-            case '-':
-                return num1 - num2;           
-            case '*':
-                return num1 * num2;     
-            case '/': 
-                return Math.round(num1 / num2 * 10) / 10;     
-        }
+  switch (key) {
+    case '+':
+      return num1 + num2; 
+    case '-':
+      return num1 - num2;           
+    case '*':
+      return num1 * num2;     
+    case '/': 
+      return Math.round(num1 / num2 * 10) / 10;     
+  }
 }
 
 function generate(): number {
-        const min = -10;
-        const max = 10;
-        const res = Math.round(Math.random()  * (max - min ) + min);
-        return res ? res : max;
+  const min = -10;
+  const max = 10;
+  const res = Math.round(Math.random()  * (max - min ) + min);
+  return res ? res : max;
  }
 async function main() {
-        while (true) {   
-            const numberFirst = generate();
-            const numberSecond = generate();
-            const key = generateKey();
-            const result = operation(numberFirst, numberSecond, key);
-            const answ = await prompt(`${numberFirst} ${key} ${numberSecond} = `);
+  while (true) {   
+    const numberFirst = generate();
+    const numberSecond = generate();
+    const key = generateKey();
+    const result = operation(numberFirst, numberSecond, key);
+    const answ = await prompt(`${numberFirst} ${key} ${numberSecond} = `);
 
-            if (answ == "!exit") {
-                console.log(`Содержание алкоголя ~${score / 10}00 промилле!`);
-                break;
-            }
-
-            if (Number(answ) == result) {
-                console.log("Наливай еще!");
-            } else {
-                score++;
-                console.log(`Хватит пить!\nВерный ответ: ${result}`);
-            }
-        }
+      if (answ == "!exit") {
+        console.log(`Содержание алкоголя ~${score / 10}00 промилле!`);
+        break;
+      }
+      if (Number(answ) == result) {
+        console.log("Наливай еще!");
+      } else {
+        score++;
+        console.log(`Хватит пить!\nВерный ответ: ${result}`);
+      }
+  }
 }
 main();
  
